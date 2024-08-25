38ce85c0ae17b20cadff3e7e767518abd3b4cbae
jaaabir <jaabir.zaid@gmail.com>
2024-08-25 19:22:47 +0530

    added the google analytics tag

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Script from 'next/script'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -13,6 +14,18 @@ export default function RootLayout({ children }) {
   return (
    <ClerkProvider>
     <html lang="en">
+      <head>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-G0NQ2G408R" strategy="afterInteractive"/>
+        <Script id='google-analytics' strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-G0NQ2G408R');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
     </ClerkProvider>
